ef5b810bdc1014602137744956a9ca95d2401983
Wade Mauger <wade_mauger@intuit.com>
2016-07-10 22:02:54 -0700

    Update

--- test.js
+++ test.js
@@ -31,4 +31,53 @@ describe('noFun\'s isFun function', function(){
   });
 });
 
+describe('noFun\'s isNotFun function', function(){
+  it('should be false if a string is "Kayla"', function(){
+    expect(nofun.isNotFun('kayla')).toBe(true);
+    expect(nofun.isNotFun('Kayla')).toBe(true);
+    expect(nofun.isNotFun('KAYLA')).toBe(true);
+  });
+  it('should be true if a string is not "Kayla"', function(){
+    expect(nofun.isNotFun('timbrook')).toBe(false);
+    expect(nofun.isNotFun('Timbrook')).toBe(false);
+    expect(nofun.isNotFun('TIMBROOK')).toBe(false);
+  });
+});
+
+describe('noFun\'s hasFun() String enhancement', function(){
+  it('should return false if \'kayla\' is in a String', function(){
+    expect('kayla'.hasFun()).toBe(false);
+    expect('Kayla'.hasFun()).toBe(false);
+    expect('KAYLA'.hasFun()).toBe(false);
+    expect('Kayla is from vermont'.hasFun()).toBe(false);
+    expect('Kayla likes coffee'.hasFun()).toBe(false);
+    expect('Our president is KAYLA (all hail)'.hasFun()).toBe(false);
+  });
+  it('should return true if \'kayla\' is not in a String', function(){
+    expect('timbrook'.hasFun()).toBe(true);
+    expect('Timbrook'.hasFun()).toBe(true);
+    expect('TIMBROOK'.hasFun()).toBe(true);
+    expect('Timbrook is from new york'.hasFun()).toBe(true);
+    expect('Timbrook probably likes coffee too'.hasFun()).toBe(true);
+    expect('Our vice president is TIMBROOK (respect)'.hasFun()).toBe(true);
+  });
+});
+
+describe('noFun\'s hasFun() Array enhancement', function(){
+  it('should return true if there are not any \'kayla\'s in an Array', function(){
+    expect(['Tim', ' ', 'Brook'].hasFun()).toBe(true);
+    expect(['Vice', 'President'].hasFun()).toBe(true);
+    expect(['Michael', 'Timbrook'].hasFun()).toBe(true);
+  });
+  it('should return false if there are one or more \'kayla\'s in an Array', function(){
+    expect(['President', 'Kayla'].hasFun()).toBe(false);
+    expect(['kayla', 'is', 'from', 'Vermont'].hasFun()).toBe(false);
+    expect(['KAYLA', 'is', 'going', 'to', 'hate', 'us'].hasFun()).toBe(false);
+  });
+});
+
+//describe('noFun\'s hasFun() Object enhancement', function(){
+//  
+//});
+
 jasmine.execute();
