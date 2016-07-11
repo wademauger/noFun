d130b7ca6bd09ef246a4f8e26ea16cb79f86a883
Wade Mauger <wade_mauger@intuit.com>
2016-07-10 20:59:22 -0700

    Fix the bug in node where arrays don't know if they're fun

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 function noFun() {
+  console.log("Kayla is not fun");
+
   this.KAYLA = "NOT FUN";
   this.FUN = "NOT KAYLA";
-  this.isFun = function(notKayla) {
+
+  this.isFun = function(notKayla){
     return notKayla.toLowerCase() !== "kayla";
   }
-  console.log("Kayla is not fun");
+
+  this.isNotFun = function(Kayla){
+    return !this.isFun(Kayla);
+  }
+
+  Array.prototype.hasFun = function(){
+    return !(this.filter(function(val){
+      return val.toLowerCase() === "kayla";
+    })).length > 0;
+  }
+
 }
 
 module.exports = new noFun();
