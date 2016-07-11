fce44f89459e3de5414c524c4ce3670ba621d8ec
Wade Mauger <wade_mauger@intuit.com>
2016-07-10 21:37:08 -0700

    Documentation

--- index.js
+++ index.js
@@ -12,10 +12,12 @@ function noFun() {
     return !this.isFun(Kayla);
   }
 
+  //returns true iff 'kayla' is not inside
   String.prototype.hasFun = function() {
     return this.toLowerCase().indexOf('kayla') === -1;
   }
 
+  //returns true iff 'kayla' is nowhere in the Array
   Array.prototype.hasFun = function () {
     return !this.find(function(el) {
       if (typeof el === 'string' || typeof el === 'object') {
@@ -26,6 +28,7 @@ function noFun() {
     }, this);
   }
 
+  //returns true iff 'layla' is not therein
   Object.prototype.hasFun = function() {
     return !Object.keys(this).find(function(el) {
       if (!el.hasFun()) {
